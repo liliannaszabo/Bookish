0e8f041b58bdbcfbc31f4245c0ae3530725655ec
Lilianna Szabo <lilianna.szabo@softwire.com>
2024-09-13 15:26:02 +0100

    create books and copies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index', to:"books#index"
+  get 'books/add', to:"books#add"
+  get 'books/remove', to:"books#remove"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "books#index"
 end
