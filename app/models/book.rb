658ac133d464811e123261439752e7ceb6fe5e60
Lilianna Szabo <lilianna.szabo@softwire.com>
2024-09-13 16:34:27 +0100

    Add CRUD operations to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   has_many :copies
-
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index', to:"books#index"
-  get 'books/add', to:"books#add"
-  get 'books/remove', to:"books#remove"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "books#index"
+  resources :books
+  root "books#index"
 end
